495c557dda1062a60be9768549678885a3da29c8
Kyle Woodruff <kylewoodruff@gmail.com>
2019-04-18 18:48:43 -0700

    initial file setup for homework activity

---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var concert = "concert-this";
+var song = "spotify-this-song";
+var movie = "moive-this";
+var doIt = "do-what-it-says";
